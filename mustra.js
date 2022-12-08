975481280ca1a6bf18b558c103247dfe6527492e
Kevin <ke.91vin@gmail.com>
2022-12-07 23:05:51 -0600

    probando resubir el proyecto de Lastro

---
+++ mustra.js
@@ -0,0 +1,11 @@
+function nosotros(){
+    location.href="nosotros.html"
+}
+
+    function catalogo(){
+    
+        location.href="catalogo.html"
+    }
+    function contacto(){
+        location.href="contacto.html"
+    }
